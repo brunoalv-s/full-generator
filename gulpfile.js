04dc21f2af47d6a78803441110350d8a80180bd2
Bruno Alves <bruno.alves@fullbar.srv>
2017-05-22 18:50:19 -0300

    adicionado index page

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,17 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
+    prefix = require('gulp-autoprefixer'),
     pug  = require('gulp-pug'),
+    plumber = require('gulp-plumber'),
     bsync = require('browser-sync');
 
 gulp.task('sass', function () {
   return gulp.src('app/assets/sass/**/*')
+    .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sass())
+    .pipe(prefix('last 2 versions', 'safari 5', 'ie6', 'ie7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(sourcemaps.write('../maps'))
     .pipe(gulp.dest('build/assets/css/'))
     .pipe(bsync.stream());
@@ -17,6 +21,7 @@ gulp.task('sass', function () {
 
 gulp.task('pug', function () {
   return gulp.src('app/*.pug')
+    .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(pug())
     .pipe(sourcemaps.write('assets/maps'))
