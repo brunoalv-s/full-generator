d9f2c30c8c14f512b46f7e437b2d9ddb7ac5fba1
Bruno Alves <bruno.alves@fullbar.srv>
2017-05-23 12:19:17 -0300

    alterações na página base

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 "use strict";
 
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    prefix = require('gulp-autoprefixer'),
-    pug  = require('gulp-pug'),
-    plumber = require('gulp-plumber'),
-    bsync = require('browser-sync');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var prefix = require('gulp-autoprefixer');
+var pug = require('gulp-pug');
+var plumber = require('gulp-plumber');
+var bsync = require('browser-sync');
+var imagemin = require('gulp-imagemin');
 
-gulp.task('sass', function () {
+gulp.task('sass', function() {
   return gulp.src('app/assets/sass/**/*')
     .pipe(plumber())
     .pipe(sourcemaps.init())
@@ -19,7 +20,7 @@ gulp.task('sass', function () {
     .pipe(bsync.stream());
 });
 
-gulp.task('pug', function () {
+gulp.task('pug', function() {
   return gulp.src('app/*.pug')
     .pipe(plumber())
     .pipe(sourcemaps.init())
@@ -29,13 +30,27 @@ gulp.task('pug', function () {
     .pipe(bsync.stream());
 });
 
-gulp.task('dev', ['pug', 'sass'], function () {
+gulp.task('img', function() {
+  gulp.src('app/assets/img/*')
+    .pipe(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.svgo({plugins: [{removeViewBox: true}]})
+    ]))
+    .pipe(gulp.dest('build/assets/img'))
+    .pipe(bsync.stream());
+});
+
+gulp.task('dev', ['pug', 'sass'], function() {
   bsync.init({
     server: './build/'
   });
 
   gulp.watch('app/assets/sass/**/*.sass', ['sass']);
   gulp.watch('app/**/*.pug', ['pug']);
+  gulp.watch('app/assets/img/**/*', ['img']);
+  gulp.watch('build/assets/img/**/*').on('change', bsync.reload);
   gulp.watch('build/**/*.{html,css,js}').on('change', bsync.reload);
 });
 
